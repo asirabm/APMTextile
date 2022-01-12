22858285db5ff7f904909f0f2c3a6a091b1282b8
asirabm <asir.abm@gmail.com>
2022-01-12 11:52:11 +0530

    share code on github

---
+++ Documents/Nodejs/APM/frontend/src/data.js
@@ -0,0 +1,80 @@
+const data={
+
+
+ products:[
+    {
+    id:'1',
+    name: "Nike Slim Shirt",
+    image: '/images/men.jpg',
+    price: '120/=',
+    brand: 'Nike',
+    rating:4.5,
+    numReviews: 10,
+    description:'High Quality item'
+    },
+    {
+        id:'2',
+        name: "Nike XL Shirt",
+        image: '/images/men.jpg',
+        price: '120/=',
+        brand: 'Nike',
+        rating:4.5,
+        numReviews: 10,
+        description:'High Quality item'
+        },
+        {
+            id:'3',
+            name: "Nike Slim Shirt",
+            image: '/images/men.jpg',
+            price: '120/=',
+            brand: 'Nike',
+            rating:4.5,
+            numReviews: 10,
+            description:'High Quality item'
+            },
+            {
+                id:'4',
+                name: "Nike Slim Shirt",
+                image: '/images/men.jpg',
+                price: '120/=',
+                brand: 'Nike',
+                rating:4.5,
+                numReviews: 10,
+                description:'High Quality item'
+                },
+                {
+                    id:'5',
+                    name: "Nike Slim Shirt",
+                    image: '/images/men.jpg',
+                    price: '120/=',
+                    brand: 'Nike',
+                    rating:4.5,
+                    numReviews: 10,
+                    description:'High Quality item'
+                    },
+                    {
+                        id:'6',
+                        name: "Nike Slim Shirt",
+                        image: '/images/men.jpg',
+                        price: '120/=',
+                        brand: 'Nike',
+                        rating:4.5,
+                        numReviews: 10,
+                        description:'High Quality item'
+                        },
+                        {
+                            id:'7gi',
+                            name: "Nike Slim Shirt",
+                            image: '/images/men.jpg',
+                            price: '120/=',
+                            brand: 'Nike',
+                            rating:4.5,
+                            numReviews: 10,
+                            description:'High Quality item'
+                            }
+   
+
+ ]
+}
+
+export default data;
